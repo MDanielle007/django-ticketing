71b9e9b754eca53304a71e17da96de7f631dab19
ITSD Dev Intern 4 <itsddev.intern4@mgenesis.com>
2025-02-26 15:27:03 +0800

    add users urls and base.html modify tickets urls to /tickets/

--- ticketing_app/urls.py
+++ ticketing_app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('tickets.urls'))
+    path('tickets',include('tickets.urls')),
+    path('', include('users.urls'))
 ]
